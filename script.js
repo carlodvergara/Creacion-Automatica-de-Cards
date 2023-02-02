7fc0c4aed96db232c6745b7fe071d0b61d6e04cf
Carlos Vergara Cardenas <carlosdvergaracardenas91@gmail.com>
2023-02-01 20:37:25 -0400

    Construccion bases  HTML,  css, Javascript

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 
+// Variables Formulario HTML
+const inputBrand = document.querySelector('input-brand');
+const inputModel = document.querySelector('input-model');
+const inputYear = document.querySelector('input-year');
+const inputImage = document.querySelector('input-image');
+const btnCreator = document.querySelector('btn-creator');
 
+
+// Variables Area Card
+const inputSearch = document.querySelector('input-search');
+
+
+// Evento click
+
+btnCreator.addEventListener('click', creationCard);
+
+creationCard(){
+
+
+}
