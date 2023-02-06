626cd54f9f89f7245e06011a7a031b382e04da2a
Carlos Vergara Cardenas <carlosdvergaracardenas91@gmail.com>
2023-02-05 21:27:23 -0400

    Reparacion Problema Duplicidad de tarjetas

--- script.js
+++ script.js
@@ -1,73 +1,87 @@
 
 // Variables Formulario HTML
-const inputBrand = document.querySelector('input-brand');
-const inputModel = document.querySelector('input-model');
-const inputYear = document.querySelector('input-year');
-const inputImage = document.querySelector('input-image');
-const btnCreator = document.querySelector('btn-creator');
+const inputBrand = document.querySelector('.input-brand');
+const inputModel = document.querySelector('.input-model');
+const inputYear = document.querySelector('.input-year');
+const inputImage = document.querySelector('.input-image');
+const btnCreator = document.querySelector('.btn-creator');
 
 
 // Variables Area Card
-const containerCards = document.querySelector('container-cards')
-const inputSearch = document.querySelector('input-search');
+const containerCards = document.querySelector('.container-cards')
+const inputSearch = document.querySelector('.input-search');
+const btnSearch = document.querySelector('.btn-search');
+
+var listOfCards = [];
 
 
-const listOfCards=[]
+function Builder(image, brand, model, year, counter){
+    this.imagen = image
+    this.marca = brand
+    this.modelo = model
+    this.año = year
+    this.contador = counter
+}
 
 
 
-btnCreator.addEventListener('click', creationCard);
+btnCreator.addEventListener('click' , actionCard);
 
 
-function creationCard(){
+let contador = 0;
 
-   
+function actionCard() {
 
-for (var element of listOfCards){
+contador++;
 
-    const bodyCard = document.createElement('div');
-    bodyCard.classList.add('body-card');   
+containerCards.innerHTML = '';
+
+let newData = new Builder(inputImage.value , inputBrand.value , inputModel.value , inputYear.value, contador);
+
+listOfCards.push(newData);
+
+
+for (element of listOfCards){    
+
+    const card = document.createElement('div');
+    card.classList.add('body-card');
 
     const img = document.createElement('img');
-    img.classList.add('image-vehicle');   
-    img.setAttribute('src' , element.imagen);
+    img.classList.add('image-vehicle');
+    img.setAttribute('src' , element.imagen );
 
     const textArea = document.createElement('div');
-    textArea.classList.add('text-area'); 
+    textArea.classList.add('text-area');
 
     const brandName = document.createElement('h3');
     brandName.classList.add('brand-name');
     brandName.innerText = element.marca;
 
     const modelName = document.createElement('h4');
-    modelName.classList.add('model-name'); 
+    modelName.classList.add('model-name');
     modelName.innerText = element.modelo;
 
-    const yearNumber = document.createElement('h4');
-    yearNumber.classList.add('year-number'); 
-    yearNumber.innerText = element.año;
+    const numberYear = document.createElement('h4');
+    numberYear.classList.add('number-year');
+    numberYear.innerText = element.año;
 
-    //Insercion de elementos
-    containerCards.appendChild(bodyCard);
-    bodyCard.appendChild(img);
-    bodyCard.appendChild(textArea);
+    containerCards.appendChild(card);
+    card.appendChild(img);
+    card.appendChild(textArea);
     textArea.appendChild(brandName);
     textArea.appendChild(modelName);
-    textArea.appendChild(yearNumber);
+    textArea.appendChild(numberYear);   
+}
+
+
 }
 
 
 
+
+inputSearch.addEventListener('click', finder);
+
+function finder(){
+    var newList = listOfCards.find((a)=> a === a.inputSearch);
+    containerCards.innerHTML = '';
 }
-
-
-
-
-
-
-
-
-
-
-
-
